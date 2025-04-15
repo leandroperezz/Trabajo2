a30c91552745ce619b6644c23e34273f1e7c97e9
LeandroPerez <leaperez435@gmail.com>
2025-04-15 12:09:01 -0300

    Tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from 'react';
-import PasswordInput from './components/PasswordInput';
-import StrengthIndicator from './components/StrengthIndicator';
-import UsernameInput from './components/UsernameInput';
 import { use } from 'react';
 
 function Contraseña(){
@@ -18,33 +15,71 @@ function Contraseña(){
   };
 
   const mostrarpass = () => {
-    mostrar(!showPassword);
+    setmostrar(!mostrar);
   };
 
   const evaluarfuerza = (pass) => {
-  let cont = 0;
+  let fuerza = 0;
   if(!pass) return '';
 
-  if(pass.length >= 8) cont++;
-  if(/[a-z]/.test(pass)) cont++;
-  if(/[A-Z]/.test(pass)) cont++;
-  if(/[\d]/.test(pass)) cont++;
-  if(/[^A-Za-Z0-9]/.test(pass)) cont++;
+  if(pass.length >= 8) fuerza++;
+  if(/[a-z]/.test(pass)) fuerza++;
+  if(/[A-Z]/.test(pass)) fuerza++;
+  if(/[\d]/.test(pass)) fuerza++;
+  if(/[^A-Za-Z0-9]/.test(pass)) fuerza++;
 
-  if(cont <= 2) return 'Contraseña poco segura.';
-  if(cont === 3 || cont === 4) return 'Constraseña segura';
-  if(cont > 5) return 'Contraseña muy segura';
-  else setfuerza('');
-  }
+  if(fuerza <= 2) return 'Contraseña poco segura.';
+  if(fuerza === 3 || fuerza === 4) return 'Constraseña segura';
+  if(fuerza > 4) return 'Contraseña muy segura';
+  };
 
   return(
-    <div className='divingreso'>
-      <h1 className='h1ingreso'>Registre su cuenta.</h1>
-      <UsernameInput user={user} onChange={actualizauser} />
-      <PasswordInput pass={pass} mostrar={mostrar} onChange={actualizapass} toggleVisibility={mostrarpass} />
-      <StrengthIndicator strength={evaluarfuerza(pass)} />
+    <div className='divregistro'>
+      <h1 className='h1registro'>Registre su cuenta.</h1>
+      <Ingresousuario user={user} onChange={actualizauser} />
+      <Ingresopass pass={pass} mostrar={mostrar} onChange={actualizapass} visibilidad={mostrarpass} />
+      <Indicafuerza strength={evaluarfuerza(pass)} />
     </div>
-  )
-}
+  );
+};
+
+const Ingresousuario = ({ user, onChange }) => {
+  return(
+    <div className='divingreso'>
+      <input
+        type="text"
+        value={user}
+        onChange={onChange}
+        className="inputdatos"
+        placeholder='Ingresa tu nomre de usuario'
+      />
+    </div>
+  );
+};
+
+const Ingresopass = ({pass, visibilidad, actualizapass, mostrarpass}) => {
+  return(
+    <div className='divingreso'>
+      <input
+      type={mostrarpass ? 'text' : 'password'}
+      value={pass}
+      onChange={actualizapass}
+      className="inputdatos"
+      placeholder="Ingresa tu Contraseña"
+      />
+    <button onClick={mostrarpass} className='mostrarpass'>
+      {visibilidad ? 'Ocultar' : 'Mostrar'} contraseña
+    </button>
+    </div>
+  );
+};
+
+const Indicafuerza = ({ fuerza }) => {
+  return(
+    <div>
+      <p className='mostrarfuerza'>Fuerza: <span>{fuerza}</span></p>
+    </div>
+  );
+};
 
 export default App;
